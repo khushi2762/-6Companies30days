b11f838393fbc0fb6b95dc27504d284ecd1afe6a
Khushi Sharma <92581650+khushi2762@users.noreply.github.com>
2022-01-02 20:32:21 +0530

    ugly number

--- que1.c
+++ que1.c
@@ -1,8 +1,53 @@
-  if(i==2||i==3||i==5)
+#include<stdio.h>
+void fact(int);
+
+int main()
+{
+    int n;
+    printf("enter the number");
+    scanf("%d",&n);
+    printf("factors %d are: ",n);
+    fact(n);
+    return 0;
+    
+    
+}
+void fact(int n)
+{
+    int i;
+
+    for(i=2;i<=n;i++)
+    {
+        if(n%i==0)
+        {     
+            
+            if(i==2||i==3||i==5)
             {
-                printf("true");
+                if(i==2&&i==3)
+                {
+                    printf("true");
+                }
+                else if(i==2&&i==5)
+                {
+                    printf("true");
+                }
+                else
+                {
+                    printf("true");
+                }
             }
             else
             {
                 printf("false");
             }
+        
+            
+           
+        }
+          
+        
+
+    }
+           
+}
+
